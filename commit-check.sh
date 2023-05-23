5ac3391880e5bee569790c36ae17ee3fabef73a5
FarisZR <35614734+FarisZR@users.noreply.github.com>
2023-05-24 01:23:40 +0300

    add commit check script (borrowed from Privacy-OCI)

---
+++ commit-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# $1=git link $2 = local hash file $3 remote/file that contains the hash in the repo $4 output name
+git ls-remote $1 HEAD > $2
+
+local="$2"
+remote="$3"
+# if the files match, then it means the build is up-to-date, so no need to rebuild.
+if cmp -s "$local" "$remote"; then
+    echo "::set-output name=$4::false"
+else
+    echo "{$4}={true}" >> $GITHUB_OUTPUT
+fi
