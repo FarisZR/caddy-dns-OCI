c65bb85123636502462fbb5b24dcf7951974dfd7
FarisZR <35614734+FarisZR@users.noreply.github.com>
2023-05-25 02:41:15 +0300

    fix typos

--- commit-check.sh
+++ commit-check.sh
@@ -6,7 +6,7 @@ local="$2"
 remote="$3"
 # if the files match, then it means the build is up-to-date, so no need to rebuild.
 if cmp -s "$local" "$remote"; then
-    echo "{$4}={false}" >> $GITHUB_OUTPUT
+    echo "$4=false" >> $GITHUB_OUTPUT
 else
-    echo "{$4}={true}" >> $GITHUB_OUTPUT
+    echo "$4=true" >> $GITHUB_OUTPUT
 fi
