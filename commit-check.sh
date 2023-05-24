a9221f691dbe47e85c89b408ea4470fdc9bff71a
FarisZR <35614734+FarisZR@users.noreply.github.com>
2023-05-25 01:23:19 +0300

    switch to new way of setting github output

--- commit-check.sh
+++ commit-check.sh
@@ -6,7 +6,7 @@ local="$2"
 remote="$3"
 # if the files match, then it means the build is up-to-date, so no need to rebuild.
 if cmp -s "$local" "$remote"; then
-    echo "::set-output name=$4::false"
+    echo "{$4}={false}" >> $GITHUB_OUTPUT
 else
     echo "{$4}={true}" >> $GITHUB_OUTPUT
 fi
